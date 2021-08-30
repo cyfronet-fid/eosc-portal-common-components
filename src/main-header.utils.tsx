2ffaec490688c5d8ae12ef9001a8afb600395f0f
danielkryska <daniel.kryska94@gmail.com>
2021-08-30 12:51:34 +0200

    [Test]

--- src/main-header.utils.tsx
+++ src/main-header.utils.tsx
@@ -35,7 +35,7 @@ export function tryLogin(props: IEoscMainHeader) {
     return;
   }
 
-  console.warn("Login attempt goes wrong. Your session may expired.");
+  console.warn("Login attempt goes wrong. Your session may expired!");
 }
 
 export function valid(props: IEoscMainHeader) {
