ae6bda1add04982b2737c66d2f4d70454acc5a13
Maria <maria77julia@gmail.com>
2024-10-30 15:27:10 +0100

    update beta env for sandbox instances, remove eu-information component

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 import { default as main_header } from "/home/eland/Desktop/eosc-portal-common/src/main-header/main-header.interface.jsx";
 
 import { default as main_footer } from "/home/eland/Desktop/eosc-portal-common/src/main-footer/main-footer.interface.jsx";
-import { default as eu_information } from "/home/eland/Desktop/eosc-portal-common/src/eu-information.interface.jsx";
 
 main_header;
 
 main_header;
 main_footer;
-eu_information;
